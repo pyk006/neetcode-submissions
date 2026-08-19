861c33fac967bbf04e139e2882cb40f20afe570d
Phil Kim <43870539+pyk006@users.noreply.github.com>
2026-08-18 20:54:45 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        start = 1
+        end = max(piles)
+        best = float('infinity')
+        while start < end:
+            mid = (start + end) // 2
+
+            total = 0
+            for i in range(len(piles)):
+                total += math.ceil(piles[i] / mid)
+            
+            if total <= h:
+                best = min(mid, best)
+                end = mid
+            else:
+                start = mid + 1
+        
+        return max(piles) if best == float('infinity') else best
